da7882a49f124142822ba142d22b1a84808c32f2
Rebecca Nayala <140104442+rebeccanayala@users.noreply.github.com>
2024-03-23 21:39:56 -0300

    Adding initial files

---
+++ src/main/java/com/store/checkout/system/storecheckoutsystem/StoreCheckoutSystemApplication.java
@@ -0,0 +1,13 @@
+package com.store.checkout.system.storecheckoutsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreCheckoutSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreCheckoutSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/store/checkout/system/storecheckoutsystem/StoreCheckoutSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.checkout.system.storecheckoutsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreCheckoutSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
